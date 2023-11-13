a099b2df61fef27c3946dda1e5a34263aa2ba05c
arseniismpx <arseniit@gmail.com>
2023-11-13 18:23:46 +0100

    add Proposed names in Contest item React component

--- src/components/Contest.tsx
+++ src/components/Contest.tsx
@@ -21,6 +21,20 @@ function Contest({ initialContest, onContestListClick }) {
         <div className='contest'>
             <div className='title'>Contest Description</div>
             <div className='description'>{contest?.description}</div>
+
+            <div className="title">Proposed names</div>
+            <div className="body">
+                {contest.names?.length > 0 ? (
+                        <div className="list">
+                            {contest.names.map((proposedName) => {
+                                return (<div key={proposedName.id} className="item">{proposedName.name}</div>)
+                            })}
+                        </div>
+                    ) : (
+                        <div className="list">No names  proposed yet</div>
+                )}
+            </div>
+
             <a href='/' className='link' onClick={handleClickContestList}>
                 Contest List
             </a>
